46a578d1ff3ee3e94fe062689bf3ee9657a37142
Tushar Dhakad <tushardhakad355@gmail.com>
2024-09-01 23:24:22 +0530

    2024.09.01

---
+++ OOAD/OOps/operator_overloading/Increment_operator.cpp/postPre_as_non_member.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class test{
+    public:
+    int data;
+    test(int a){
+        this->data=a;
+    }
+    void print(){
+        cout<<data<<endl;
+    }
+};
+test operator++(test &t){
+    t.data++;
+    return t;
+}
+test operator++(test &t,int){
+    test temp=t;
+    t.data++;
+    return temp;
+}
+int main(){
+    test t(10);
+    t.print();
+    test t1=t++;
+    t1.print();
+    t1=++t;
+    t.print();
+    t1.print();
+}
